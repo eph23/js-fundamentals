68776b8a095c8a62f85bcfe80bba2275f58d2e1c
ephraim <ephraims.818@gmail.com>
2024-11-20 05:23:53 +0600

    JS FUNDAMENTALS - 01. Functions

---
+++ 01. Functions/script.js
@@ -0,0 +1,21 @@
+"use strict";
+// Function Declaration
+function logger() {
+    console.log("My name is Ephraim");
+}
+
+// Calling/invoking/running the function
+logger();
+
+// Function with parameters
+function fruitProcessor(apples, oranges) {
+    console.log(apples, oranges);
+    const juice = `Juice with ${apples} apples and ${oranges} oranges`;
+    return juice;
+}
+
+const appleJuice = fruitProcessor(5, 0);
+console.log(appleJuice);
+
+const appleOrangeJuice = fruitProcessor(2, 4);
+console.log(appleOrangeJuice);
